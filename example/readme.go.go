a88ea9b3f9cf89dda6f4c3cdc4ed9dbfbee7e494
Tristan Rice <rice@fn.lc>
2015-03-27 09:42:33 -0700

    Removed generated pry files

--- example/readme.go.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/d4l3k/go-pry/pry"
-
-func main() {
-	a := 1
-	pry.Apply(map[string]interface{}{ "main": main, "a": a, "pry": pry.Package{Name: "pry", Functions: map[string]interface{}{"Package": pry.Package{}, "Pry": pry.Pry, "Apply": pry.Apply, "Append": pry.Append, "Make": pry.Make, "Highlight": pry.Highlight, "HighlightWords": pry.HighlightWords, "InterpretExpr": pry.InterpretExpr, "StringToType": pry.StringToType, "ValuesToInterfaces": pry.ValuesToInterfaces, "Scope": pry.Scope{}, "InterpretString": pry.InterpretString, "ComputeBinaryOp": pry.ComputeBinaryOp, "ComputeUnaryOp": pry.ComputeUnaryOp, }}, })
-
-}
