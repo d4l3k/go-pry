4022dc7784c08318c39d634e7fdbdbb45ed24a8a
Tristan Rice <rice@fn.lc>
2016-12-25 13:30:00 -0800

    pry: go-pry now properly works on go1.5

---
+++ pry/importer.go
@@ -0,0 +1,30 @@
+// +build !go1.5
+
+package pry
+
+import (
+	"go/types"
+
+	gcimporter "golang.org/x/tools/go/gcimporter15"
+)
+
+var gcImporter = importer{
+	impFn:    gcimporter.Import,
+	packages: make(map[string]*types.Package),
+}
+
+// importer implements go/types.Importer.
+// It also implements go/types.ImporterFrom, which was new in Go 1.6,
+// so vendoring will work.
+type importer struct {
+	impFn    func(packages map[string]*types.Package, path, srcDir string) (*types.Package, error)
+	packages map[string]*types.Package
+}
+
+func (i importer) Import(path string) (*types.Package, error) {
+	return i.impFn(i.packages, path, "")
+}
+
+func (i importer) ImportFrom(path, srcDir string, mode types.ImportMode) (*types.Package, error) {
+	return i.impFn(i.packages, path, srcDir)
+}
