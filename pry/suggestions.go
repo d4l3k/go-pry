c936335e3da1b28485a108324322613fa9cc508f
Tristan Rice <rice@fn.lc>
2018-09-20 21:49:55 -0700

    pry/suggestions: autocomplete struct methods and fields

--- pry/suggestions.go
+++ pry/suggestions.go
@@ -1,7 +1,7 @@
 package pry
 
 import (
-	"log"
+	"reflect"
 	"regexp"
 	"sort"
 	"strings"
@@ -50,27 +50,59 @@ type getter interface {
 }
 
 func get(v interface{}, key string) (interface{}, bool) {
+	if v == nil {
+		return nil, false
+	}
+
 	g, ok := v.(getter)
 	if ok {
 		return g.Get(key)
 	}
 
-	switch v := v.(type) {
-	default:
-		log.Fatalf("can't handle get for %T: %+v", v, v)
-		return nil, false
+	val := reflect.ValueOf(v)
+
+	typ := val.Type()
+	switch typ.Kind() {
+	case reflect.Ptr:
+		return get(val.Elem().Addr(), key)
+
+	case reflect.Struct:
+		if _, ok := typ.FieldByName(key); !ok {
+			return nil, false
+		}
+		return val.FieldByName(key).Interface(), true
 	}
+
+	return nil, false
 }
 
 func keys(v interface{}) []string {
+	if v == nil {
+		return nil
+	}
+
 	g, ok := v.(keyser)
 	if ok {
 		return g.Keys()
 	}
 
-	switch v := v.(type) {
-	default:
-		log.Fatalf("can't handle keys for %T: %+v", v, v)
-		return nil
+	val := reflect.ValueOf(v)
+
+	typ := val.Type()
+	switch typ.Kind() {
+	case reflect.Ptr:
+		return keys(val.Elem().Addr())
+
+	case reflect.Struct:
+		var keys []string
+		for i := 0; i < typ.NumField(); i++ {
+			keys = append(keys, typ.Field(i).Name)
+		}
+		for i := 0; i < typ.NumMethod(); i++ {
+			keys = append(keys, typ.Method(i).Name)
+		}
+		return keys
 	}
+
+	return nil
 }
